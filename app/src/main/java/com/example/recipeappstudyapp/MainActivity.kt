1ade721ab848e6d80f487eb41a94f3307cac665a
Pether <petherjs@motorola.com>
2021-02-03 18:26:39 -0300

    adding a button and improving layout

--- app/src/main/java/com/example/recipeappstudyapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeappstudyapp/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.imageFromResource
@@ -21,42 +23,53 @@ class MainActivity : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		setContent {
 			ScrollableColumn(
-				modifier = Modifier
-					.background(color = Color(0xFFF2F2F2))
-					.fillMaxSize()
+					modifier = Modifier
+							.background(color = Color(0xFFF2F2F2))
+							.fillMaxSize()
 			) {
 				Image(
-					bitmap = imageFromResource(
-						res = resources,
-						R.drawable.happy_meal_small
-					),
-					modifier = Modifier.height(300.dp),
-					contentScale = ContentScale.Crop
+						bitmap = imageFromResource(
+								res = resources,
+								R.drawable.happy_meal_small
+						),
+						modifier = Modifier.height(300.dp),
+						contentScale = ContentScale.Crop
 				)
 				Column(
-					modifier = Modifier.padding(16.dp)
+						modifier = Modifier.padding(16.dp)
 				) {
-					Text(
-						text = "Happy Meal",
-						style = TextStyle(
-							fontSize = TextUnit.Companion.Sp(26)
+					Row(modifier = Modifier.fillMaxWidth(),
+							horizontalArrangement = Arrangement.SpaceBetween
+
+					) {
+						Text(
+								text = "Happy Meal",
+								style = TextStyle(
+										fontSize = TextUnit.Companion.Sp(26)
+								)
 						)
-					)
+						Text(
+								text = "$5.99",
+								style = TextStyle(
+										fontSize = TextUnit.Companion.Sp(17),
+										color = Color(0xFF85bb65),
+								),
+								modifier = Modifier.align(Alignment.CenterVertically)
+						)
+					}
 					Spacer(modifier = Modifier.padding(top = 10.dp))
 					Text(
-						text = "800 Calories",
-						style = TextStyle(
-							fontSize = TextUnit.Companion.Sp(17)
-						)
+							text = "800 Calories",
+							style = TextStyle(
+									fontSize = TextUnit.Companion.Sp(17)
+							)
 					)
 					Spacer(modifier = Modifier.padding(top = 10.dp))
-					Text(
-						text = "$5.99",
-						style = TextStyle(
-							fontSize = TextUnit.Companion.Sp(17),
-							color = Color(0xFF85bb65)
-						)
-					)
+					Button(onClick = {},
+							modifier = Modifier.align(Alignment.CenterHorizontally)
+					) {
+						Text(text = "Order Now")
+					}
 				}
 			}
 		}
